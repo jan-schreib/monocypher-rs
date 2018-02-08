a53f9064bb32463c5b57503aaef387a21fc72dbd
Jan S <js@ciphron.de>
2018-02-08 12:27:33 +0100

    added missing chacha20 function and test
    added tests for lock and unlock functions

--- src/chacha20.rs
+++ src/chacha20.rs
@@ -1,10 +1,11 @@
+//! Chacha20 encryption functions
+
 use ffi;
 use std::mem;
 
 pub struct ChaCha20(ffi::crypto_chacha_ctx);
 
 impl ChaCha20 {
-
     #[inline]
     pub fn new(key: &[u8], nonce: [u8; 8]) -> ChaCha20 {
         unsafe {
@@ -45,5 +46,28 @@ impl ChaCha20 {
         unsafe {
             ffi::crypto_chacha20_set_ctr(&mut self.0, ctr);
         }
+    }
+}
+
+/// HChacha20 special-purpose hashing
+pub fn chacha20_h(key: [u8; 32], input: [u8; 16]) -> [u8; 32]{
+    unsafe {
+        let mut out: [u8; 32] = mem::uninitialized();
+        ffi::crypto_chacha20_H(out.as_mut_ptr(), key.as_ptr(), input.as_ptr());
+        out
+    }
+}
+
+mod test {
+    use super::chacha20_h;
+
+    #[test]
+    fn chacha20_h_test() {
+        let res: [u8; 32] = [171, 107, 219, 186,  0, 173, 209,  50,
+                             252,  77,  93,  85, 99, 106, 222, 162,
+                             122, 140, 150, 228, 61,  93, 186, 251,
+                             45,   23, 222, 14, 121, 172, 147, 241];
+
+        assert_eq!(chacha20_h([1u8; 32], [2u8;16]), res)
     }
 }
--- src/crypto.rs
+++ src/crypto.rs
@@ -75,6 +75,31 @@ mod test {
     use super::*;
 
     #[test]
+    fn lock_unlock_test() {
+        let plaintext = "secret";
+        let key: [u8; 32] = [1; 32];
+        let nonce: [u8; 24] = [2; 24];
+
+        let cymac = lock(plaintext.as_bytes(), key, nonce);
+        let clear = unlock(&cymac.0, key, nonce, cymac.1).unwrap();
+
+        assert_eq!(&String::from_utf8(clear).unwrap(), plaintext)
+    }
+
+    #[test]
+    fn aead_lock_unlock_test() {
+        let plaintext = "secret";
+        let ad = "add";
+        let key: [u8; 32] = [1; 32];
+        let nonce: [u8; 24] = [2; 24];
+
+        let cymac = aead_lock(plaintext.as_bytes(), key, nonce, ad.as_bytes());
+        let clear = aead_unlock(&cymac.0, key, nonce, cymac.1, ad.as_bytes()).unwrap();
+
+        assert_eq!(&String::from_utf8(clear).unwrap(), plaintext)
+    }
+
+    #[test]
     fn wipe_test() {
         let mut a: [u8; 16] = [0; 16];
 
